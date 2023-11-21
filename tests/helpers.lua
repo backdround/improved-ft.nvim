62e7e18387b1c993711c68bba3dcbfbff33843fd
Vlad Chepaykyn <backdround@yandex.ru>
2023-11-21 18:41:13 +0300

    Add oneline-jump tests

--- tests/helpers.lua
+++ tests/helpers.lua
@@ -1,12 +1,9 @@
 local M = {}
 
-M.set_current_buffer = function(text)
+---@return string[]
+M.get_user_lines = function(text)
   text = text or ""
   local lines = vim.fn.split(text, "\n")
-
-  -- Clear current buffer
-  local last_line_index = vim.api.nvim_buf_line_count(0)
-  vim.api.nvim_buf_set_lines(0, 0, last_line_index, true, {})
 
   -- Remove first line if empty
   if lines[1] and lines[1]:match("^[%s]*$") then
@@ -32,11 +29,37 @@ M.set_current_buffer = function(text)
     lines[i] = line:sub(min_prepending_gap + 1)
   end
 
-  vim.api.nvim_buf_set_lines(0, 0, 1, true, lines)
+  return lines
+end
+
+M.set_current_buffer = function(text)
+  local lines = M.get_user_lines(text)
+
+  -- Set current buffer
+  local last_line_index = vim.api.nvim_buf_line_count(0)
+  vim.api.nvim_buf_set_lines(0, 0, last_line_index, true, lines)
+end
+
+M.reset_mode = function()
+  local escape = vim.api.nvim_replace_termcodes("<esc>", true, false, true)
+  vim.api.nvim_feedkeys(escape, "nx", false)
+end
+
+M.reset_last_selected_region = function()
+  vim.api.nvim_buf_set_mark(0, "<", 0, 0, {})
+  vim.api.nvim_buf_set_mark(0, ">", 0, 0, {})
 end
 
 M.set_cursor = function(line, column)
   vim.api.nvim_win_set_cursor(0, {line, column})
+end
+
+M.perform_through_keymap = function(func, additional_keys)
+  local map_label = "<Plug>(perform_through_keymap)"
+  vim.keymap.set({ "n", "o", "x" }, map_label, func)
+  local keys = map_label .. additional_keys
+  keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+  vim.api.nvim_feedkeys(keys, "x", false)
 end
 
 return M
---
+++ tests/specs/oneline_jump_spec.lua
@@ -0,0 +1,94 @@
+local ft = require("improved-ft")
+local h = require("tests.helpers")
+
+require("tests.custom-asserts").register()
+
+local function get_preset(buffer_text, cursor_position)
+  return function()
+    h.reset_mode()
+    h.reset_last_selected_region()
+
+    h.set_current_buffer(buffer_text)
+    h.set_cursor(unpack(cursor_position))
+  end
+end
+
+describe("oneline-jump", function()
+  before_each(get_preset([[
+    a = some | words here
+  ]], { 1, 9 }))
+
+  describe("normal-mode", function()
+    it("forward", function()
+      h.perform_through_keymap(ft.to_char_forward, "h")
+      assert.cursor_at(1, 17)
+    end)
+
+    it("pre-forward", function()
+      h.perform_through_keymap(ft.to_pre_char_forward, "h")
+      assert.cursor_at(1, 16)
+    end)
+
+    it("backward", function()
+      h.perform_through_keymap(ft.to_char_backward, "=")
+      assert.cursor_at(1, 2)
+    end)
+
+    it("pre-backward", function()
+      h.perform_through_keymap(ft.to_pre_char_backward, "=")
+      assert.cursor_at(1, 3)
+    end)
+  end)
+
+  describe("visual-mode", function()
+    before_each(function()
+      vim.api.nvim_feedkeys("v", "n", false)
+    end)
+
+    it("forward", function()
+      h.perform_through_keymap(ft.to_char_forward, "h<esc>")
+      assert.last_selected_region({ 1, 9 }, { 1, 17 })
+    end)
+
+    it("pre-forward", function()
+      h.perform_through_keymap(ft.to_pre_char_forward, "h<esc>")
+      assert.last_selected_region({ 1, 9 }, { 1, 16 })
+    end)
+
+    it("backward", function()
+      h.perform_through_keymap(ft.to_char_backward, "=<esc>")
+      assert.last_selected_region({ 1, 2 }, { 1, 9 })
+    end)
+
+    it("pre-backward", function()
+      h.perform_through_keymap(ft.to_pre_char_backward, "=<esc>")
+      assert.last_selected_region({ 1, 3 }, { 1, 9 })
+    end)
+  end)
+
+  describe("operator-pending-mode", function()
+    before_each(function()
+      vim.api.nvim_feedkeys("d", "n", false)
+    end)
+
+    it("forward", function()
+      h.perform_through_keymap(ft.to_char_forward, "h")
+      assert.buffer("a = some ere")
+    end)
+
+    it("pre-forward", function()
+      h.perform_through_keymap(ft.to_pre_char_forward, "h")
+      assert.buffer("a = some here")
+    end)
+
+    it("backward", function()
+      h.perform_through_keymap(ft.to_char_backward, "=")
+      assert.buffer("a | words here")
+    end)
+
+    it("pre-backward", function()
+      h.perform_through_keymap(ft.to_pre_char_backward, "=")
+      assert.buffer("a =| words here")
+    end)
+  end)
+end)
